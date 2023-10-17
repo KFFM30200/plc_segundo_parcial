d840bf4320f978383c1e862e9ff68a0b23ddd082
KFFM30200 <145489523+KFFM30200@users.noreply.github.com>
2023-10-17 09:09:31 -0600

    reto prestamos.py

---
+++ reto prestamos.py
@@ -0,0 +1,24 @@
+https://replit.com/join/hfjuagurlo-karla-fernanda7 
+#Calculadora de prestamos
+print("Vamos a calcular tus prestamos")
+Cantidad = float(input("Cantidad del prestamo"))
+Tasa_de_interes = float(input("Tasa de interes anual"))
+Plazo = float(input("Plazo del prestamo")) 
+if Tasa_de_interes <5 and Plazo <= 5:
+  print("Prestamo de bajo riesgo")
+elif Tasa_de_interes > 5 and Plazo > 5:
+  print("Prestamo de riesgo moderado")
+else:
+  print("Prestamo de alto riesgo")
+  #Calculadora de prestamos
+print("Vamos a calcular tus prestamos")
+Cantidad = float(input("Cantidad del prestamo"))
+Tasa_de_interes = float(input("Tasa de interes anual"))
+Plazo = float(input("Plazo del prestamo")) 
+if Tasa_de_interes <5 and Plazo <= 5:
+  print("Prestamo de bajo riesgo")
+elif Tasa_de_interes > 5 and Plazo > 5:
+  print("Prestamo de riesgo moderado")
+else:
+  print("Prestamo de alto riesgo")
+  
